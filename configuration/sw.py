1ca7ab63f0aa7bc0eb05c45c3b1ade2a656b53eb
Caleb Smith <caleb@cern.ch>
2017-07-16 21:26:16 +0200

    adding testbeam file and fed

--- configuration/sw.py
+++ configuration/sw.py
@@ -61,15 +61,17 @@ def crateList(usc=True):
     else:  # B904
         return [50, 51, 52, 53, 61, 62, 63]
 
-
+# Use file name to define FED number.
 def set_default_feds1(options):
     if options.feds1 == "HCAL":
         if "B904" in options.file1:
             options.feds1 = "B904"
         elif "FNAL" in options.file1:
             options.feds1 = "WH14"
+        elif "HTB" in options.file1:
+            options.feds1 = "HTB"
 
-
+# FED numbers based on file name.
 def fedMap():
     d = {"7xy": range(700, 732),
          "HO": range(724, 732),
@@ -78,6 +80,7 @@ def fedMap():
          "HC": [1134, 1135],
          "B904": range(1180, 1188) + [1192, 1194, 1196],
          "WH14": [1776],
+         "HTB": [938],
          }
     d["HBEF"] = d["HBHE"] + d["HF"]
     d["HCAL"] = d["HBHE"] + d["HF"] + d["HO"] + d["HC"]
